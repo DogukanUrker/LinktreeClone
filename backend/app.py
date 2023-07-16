88da94dadea7272babcc4b93a7ba9d929195bd80
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-16 12:05:50 +0300

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_origins=origins,
+    allow_credentials=True,   
+)
+
+
+@app.get("/data")
+def message():
+    return {"message": "This message came from"}
