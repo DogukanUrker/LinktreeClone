9cd18a90e9f914bed646dcc682331c9e71ddab50
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-18 21:50:48 +0300

    db format changed

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,10 @@ import jwt
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from secrets import token_urlsafe
-from models import LoginItem, User
-from database import createUser, fetchUser
+from models import LoginItem, User, Link
 from passlib.hash import sha256_crypt
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 app = FastAPI()
 
@@ -25,26 +26,56 @@ app.add_middleware(
 )
 
 
+client = MongoClient("mongodb://localhost:27017/", server_api=ServerApi("1"))
+db = client.LinktreeClone
+colUsers = db.user
+colLinks = db.link
+
+
 @app.post("/userRegister/", response_model=User)
 async def userRegister(user: User):
-    userCheck = await fetchUser(user.username)
-    if not userCheck:
+    if not colUsers.find_one({"username": user.username}):
         user.password = sha256_crypt.hash(user.password)
-        return await createUser(user.dict())
+        return colUsers.insert_one(user.dict())
     raise HTTPException(400, "This user already exists.")
 
 
 @app.post("/login")
 async def login(loginitem: LoginItem):
     data = jsonable_encoder(loginitem)
-    response = await fetchUser(data["username"])
+    response = colUsers.find_one({"username": data["username"]})
     if response:
-        match data["username"] == response["username"]:
+        match data["username"] != "edit":
             case True:
-                match sha256_crypt.verify(data["password"], response["password"]):
+                match data["username"] == response["username"]:
                     case True:
-                        encodedJWT = jwt.encode(data, SECERT_KEY, algorithm=ALGORITHM)
-                        return {"token": encodedJWT, "user":data["username"]}
-                    case False:
-                        raise HTTPException(404, f"wrong password")
+                        match sha256_crypt.verify(
+                            data["password"], response["password"]
+                        ):
+                            case True:
+                                encodedJWT = jwt.encode(
+                                    data, SECERT_KEY, algorithm=ALGORITHM
+                                )
+                                return {"token": encodedJWT, "user": data["username"]}
+                            case False:
+                                raise HTTPException(404, f"wrong password")
+            case False:
+                raise HTTPException(404, f"user name cant be 'edit' ")
     raise HTTPException(404, f"user not found")
+
+
+@app.put("/addLink", response_model=Link)
+async def addLink(link: Link):
+    colLinks.insert_one(link.dict())
+
+
+@app.get("/fetchLinks/{username}")
+async def fetchLinks(username):
+    response = colLinks.find({"author": username})
+    data = []
+    for i in response:
+        del i["_id"]
+        data.append(dict(i))
+    if data:
+        return data
+    raise HTTPException(404, f"data not found")
