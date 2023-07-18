3c5489bf192b309e789d879a881ab087784944b3
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-18 22:02:30 +0300

    Create edit.tsx

---
+++ frontend/src/views/edit.tsx
@@ -0,0 +1,9 @@
+export default function Edit() {
+  return (
+    <>
+      <div className="container mx-auto select-none w-fit text-center mt-10">
+        <h1>Edit</h1>
+      </div>
+    </>
+  );
+}
