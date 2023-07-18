cd81d4355685e80f1d7074dc458eed1051d01a4c
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-18 22:02:32 +0300

    Create links.tsx

---
+++ frontend/src/views/links.tsx
@@ -0,0 +1,9 @@
+export default function Links() {
+  return (
+    <>
+      <div className="container mx-auto select-none w-fit text-center mt-10">
+        <h1>Links</h1>
+      </div>
+    </>
+  );
+}
