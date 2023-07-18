8a541980240798cc388a4d7638b70342b09077f2
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-18 12:08:15 +0300

    added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,16 @@ import Login from "./views/login";
 import Register from "./views/register";
 import Profile from "./views/profile";
 import { RequireToken } from "./utils/auth";
-
+import Navbar from "./components/navbar";
 function App() {
   return (
     <>
       {" "}
       <div className="App">
+        <Navbar></Navbar>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
           <Route
             path="/profile"
             element={
---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import { fetchToken } from "../utils/auth";
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  const Logout = () => {
+    localStorage.removeItem("Token");
+    navigate("/");
+  };
+  return (
+    <>
+      <nav className="relative px-4 py-4 flex justify-between items-center bg-white max-w-6xl mx-auto">
+        <a className="text-3xl font-bold leading-none" href="/">
+          <h1>Linktree Clone</h1>
+        </a>
+        {fetchToken() ? (
+          <div>
+            <button
+              className="inline-block ml-auto mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-md transition duration-200"
+              onClick={Logout}
+            >
+              sign out
+            </button>
+            <a
+              className="inline-block py-2 px-6 bg-emerald-500 hover:bg-emerald-600 text-sm text-white font-bold rounded-md transition duration-200"
+              href="/user"
+            >
+              username
+            </a>
+          </div>
+        ) : (
+          <div>
+            <a
+              className="inline-block ml-auto mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-md transition duration-200"
+              href="/login"
+            >
+              Login
+            </a>
+            <a
+              className="inline-block py-2 px-6 bg-emerald-500 hover:bg-emerald-600 text-sm text-white font-bold rounded-md transition duration-200"
+              href="/register"
+            >
+              Sign up
+            </a>
+          </div>
+        )}
+      </nav>
+    </>
+  );
+}
