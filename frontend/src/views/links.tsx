d3a8a49edd8b4c10ccd17f0442a43072be489e00
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-22 18:12:36 +0300

    Delete links.tsx

--- frontend/src/views/links.tsx
+++
@@ -1,9 +0,0 @@
-export default function Links() {
-  return (
-    <>
-      <div className="container mx-auto select-none w-fit text-center mt-10">
-        <h1>Links</h1>
-      </div>
-    </>
-  );
-}
