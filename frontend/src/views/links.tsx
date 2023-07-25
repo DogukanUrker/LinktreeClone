1daccb201a6f16012c8b6eac32f7acaa7f99b2e6
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-25 12:04:57 +0300

    links styled

--- frontend/src/views/links.tsx
+++ frontend/src/views/links.tsx
@@ -9,16 +9,16 @@ export default function Profile() {
       .get("http://localhost:8000/fetchLinks/test")
       .then((json) => setData(json.data));
   }, []);
-
   const renderLinks = () => {
     return data.map((link) => {
       return (
-        <div>
-          <p>{link.author}</p>
-          <p>{link.text}</p>
-          <p>{link.link}</p>
-          <p>{link.bgColor}</p>
-          <p>{link.textColor}</p>
+        <div
+          className="w-36 p-4 text-center m-4 rounded-md transition duration-150 ease-in-ou"
+          style={{ backgroundColor: link.bgColor }}
+        >
+          <a href={link.link} target="_blank" style={{ color: link.textColor }}>
+            {link.text}
+          </a>
         </div>
       );
     });
