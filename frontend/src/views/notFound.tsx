e248cf6de5f0f35f3f3512c25bc9319c0d629ede
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-26 01:12:03 +0300

    styled 404 page

--- frontend/src/views/notFound.tsx
+++ frontend/src/views/notFound.tsx
@@ -1,9 +1,9 @@
 export default function NotFound() {
   return (
     <>
-      <div className="w-fit text-center select-none mx-auto mt-32 font-bold text-red-600">
-        <p className="text-9xl">404</p>
-        <p className="text-4xl">Not Found</p>
+      <div className="w-fit text-center select-none mx-auto mt-32 font-bold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-600">
+        <p className="text-[12rem]">404</p>
+        <p className="text-[3rem]">Not Found</p>
       </div>
     </>
   );
