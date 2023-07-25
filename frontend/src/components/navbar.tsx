1b915bbb86d4aee3c8ead6ce615f09d2c2512803
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-25 21:53:54 +0300

    now navbar hidden in links page

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -6,44 +6,47 @@ export default function Navbar() {
     localStorage.removeItem("Token");
     navigate("/");
   };
-  return (
-    <>
-      <nav className="relative px-4 py-4 flex justify-between items-center bg-white max-w-6xl mx-auto">
-        <a className="text-3xl font-bold leading-none" href="/">
-          <h1>Linktree Clone</h1>
-        </a>
-        {fetchToken() ? (
-          <div>
-            <button
-              className="inline-block ml-auto mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-md transition duration-200"
-              onClick={Logout}
-            >
-              sign out
-            </button>
-            <a
-              className="inline-block py-2 px-6 bg-indigo-500 hover:bg-indigo-600 text-sm text-white font-bold rounded-md transition duration-200"
-              href="/profile"
-            >
-              {fetchUser()}
-            </a>
-          </div>
-        ) : (
-          <div>
-            <a
-              className="inline-block ml-auto mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-md transition duration-200"
-              href="/login"
-            >
-              Login
-            </a>
-            <a
-              className="inline-block py-2 px-6 bg-indigo-500 hover:bg-indigo-600 text-sm text-white font-bold rounded-md transition duration-200"
-              href="/register"
-            >
-              Sign up
-            </a>
-          </div>
-        )}
-      </nav>
-    </>
-  );
+  const url = window.location.pathname;
+  if (!url.includes("links")) {
+    return (
+      <>
+        <nav className="relative px-4 py-4 flex justify-between items-center bg-white max-w-6xl mx-auto">
+          <a className="text-3xl font-bold leading-none" href="/">
+            <h1>Linktree Clone</h1>
+          </a>
+          {fetchToken() ? (
+            <div>
+              <button
+                className="inline-block ml-auto mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-md transition duration-200"
+                onClick={Logout}
+              >
+                sign out
+              </button>
+              <a
+                className="inline-block py-2 px-6 bg-indigo-500 hover:bg-indigo-600 text-sm text-white font-bold rounded-md transition duration-200"
+                href="/profile"
+              >
+                {fetchUser()}
+              </a>
+            </div>
+          ) : (
+            <div>
+              <a
+                className="inline-block ml-auto mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-md transition duration-200"
+                href="/login"
+              >
+                Login
+              </a>
+              <a
+                className="inline-block py-2 px-6 bg-indigo-500 hover:bg-indigo-600 text-sm text-white font-bold rounded-md transition duration-200"
+                href="/register"
+              >
+                Sign up
+              </a>
+            </div>
+          )}
+        </nav>
+      </>
+    );
+  }
 }
