7b1d5456f68187215865e420e55037af26377ee2
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-25 23:12:43 +0300

    added random gradient backgrounds to 'links' page

--- frontend/src/views/links.tsx
+++ frontend/src/views/links.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-
+import { colorL, colorR } from "../utils/randomColor";
 export default function Links() {
   let { userName } = useParams();
   const [data, setData] = useState([]);
@@ -16,7 +16,7 @@ export default function Links() {
         <a
           href={link.link}
           target="_blank"
-          className="w-36 p-4 text-center m-4 rounded-md transition duration-150 ease-in-ou block font-medium text-lg"
+          className="w-36 p-4 m-4 rounded-md transition duration-150 ease-in-ou block font-medium text-lg"
           style={{ backgroundColor: link.bgColor, color: link.textColor }}
         >
           {link.text}
@@ -27,8 +27,17 @@ export default function Links() {
 
   return (
     <>
-      <div className=" container  grid h-screen place-items-center w-fit mx-auto">
-        <div>{renderLinks()}</div>
+      <div
+        className={`bg-gradient-to-r ${colorL} ${colorR} select-none text-center`}
+      >
+        <div className="absolute left-[50%] mt-8">
+          <h1 className="font-medium text-4xl relative left-[-50%]">
+            {userName}
+          </h1>
+        </div>
+        <div className=" container  grid h-screen place-items-center w-fit mx-auto">
+          <div>{renderLinks()}</div>
+        </div>
       </div>
     </>
   );
