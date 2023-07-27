a622a5b297a26932143627f1ea637727efef6ee9
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-27 20:36:59 +0300

    link deletion added

--- frontend/src/views/profile.tsx
+++ frontend/src/views/profile.tsx
@@ -39,13 +39,30 @@ export default function Profile() {
               className="w-fit p-2 m-2 border-slate-400 border-2 rounded-md outline-indigo-500"
               value={link.textColor}
             />
+            <button
+              onClick={() => deleteLink(link.text)}
+              className="p-2 bg-red-500 rounded-md text-white hover:bg-red-600 transtion duration-150"
+            >
+              Delete
+            </button>
           </form>
         </div>
       );
     });
   };
+  const deleteLink = (linkText: string) => {
+    axios
+      .delete(`http://localhost:8000/deleteLink/${linkText}`)
+      .then(function (response) {
+        console.log(response);
+        navigate("/profile");
+      })
+      .catch(function (error) {
+        console.log(error, "error");
+      });
+  };
   const addLink = () => {
-    if (link == "" && text == "" && bgColor == "" && textColor == "") {
+    if (!text) {
       return;
     } else {
       axios
@@ -65,44 +82,47 @@ export default function Profile() {
         });
     }
   };
+  const addLinkComponent = () => {
+    return (
+      <form>
+        <input
+          type="text"
+          className="w-fit p-2 m-2 border-slate-400 border-2 rounded-md outline-indigo-500"
+          placeholder="Text"
+          onChange={(event) => setText(event.target.value)}
+        />
+        <input
+          type="text"
+          className="w-fit p-2 m-2 border-slate-400 border-2 rounded-md outline-indigo-500"
+          placeholder="URL"
+          onChange={(event) => setLink(event.target.value)}
+        />
+        <input
+          type="text"
+          className="w-fit p-2 m-2 border-slate-400 border-2 rounded-md outline-indigo-500"
+          placeholder="Text Color HEX"
+          onChange={(event) => setTextColor(event.target.value)}
+        />
+        <input
+          type="text"
+          className="w-fit p-2 m-2 border-slate-400 border-2 rounded-md outline-indigo-500"
+          placeholder="Background Color HEX"
+          onChange={(event) => setBGColor(event.target.value)}
+        />
+        <button
+          onClick={addLink}
+          className="p-2 bg-indigo-500 rounded-md text-white hover:bg-indigo-600 transtion duration-150"
+        >
+          Add Link
+        </button>
+      </form>
+    );
+  };
   return (
     <>
-      <div className="mt-4 w-fit m-auto">
-        <h1 className="text-2xl text-center select-none">Add New Link</h1>
-        <form>
-          <input
-            type="text"
-            className="w-fit p-2 m-2 border-slate-400 border-2 rounded-md outline-indigo-500"
-            placeholder="text"
-            onChange={(event) => setText(event.target.value)}
-          />
-          <input
-            type="text"
-            className="w-fit p-2 m-2 border-slate-400 border-2 rounded-md outline-indigo-500"
-            placeholder="link"
-            onChange={(event) => setLink(event.target.value)}
-          />
-          <input
-            type="text"
-            className="w-fit p-2 m-2 border-slate-400 border-2 rounded-md outline-indigo-500"
-            placeholder="textColor"
-            onChange={(event) => setBGColor(event.target.value)}
-          />
-          <input
-            type="text"
-            className="w-fit p-2 m-2 border-slate-400 border-2 rounded-md outline-indigo-500"
-            placeholder="bgcolor"
-            onChange={(event) => setTextColor(event.target.value)}
-          />
-          <button
-            onClick={addLink}
-            className="p-2 bg-indigo-500 rounded-md text-white hover:bg-indigo-600 transtion duration-150"
-          >
-            Add Link
-          </button>
-        </form>
+      <div className="mt-[5%] w-fit mx-auto">
+        {renderLinks()} {addLinkComponent()}
       </div>
-      <div className="mt-4 w-fit mx-auto mt-4">{renderLinks()}</div>
     </>
   );
 }
