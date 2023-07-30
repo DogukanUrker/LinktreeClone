854dac4bf89c49d1331be2f02af2916ed7cdd4b8
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-31 00:52:38 +0300

    "Click for create link pages" --> "Click for create custom link page"

--- frontend/src/views/index.tsx
+++ frontend/src/views/index.tsx
@@ -3,7 +3,13 @@ export default function index() {
   return (
     <>
       <div className="w-fit mx-auto select-none">
-        <img src={image} alt="" className="w-72 mt-12 lg:w-96 h-fit mb-4" />
+        <a href="/links/DogukanUrker" target="_blank">
+          <img
+            src={image}
+            alt="DogukanUrker's Links"
+            className="w-72 mt-12 lg:w-96 h-fit mb-4"
+          />
+        </a>
       </div>
       <div className="w-fit mx-auto">
         {" "}
@@ -11,7 +17,7 @@ export default function index() {
           href="/register"
           className="text-center text-xl text-indigo-500 hover:text-indigo-600 rounded-lg  duration-150 p-2"
         >
-          Click for create link pages
+          Click for create custom link page
         </a>
       </div>
     </>
