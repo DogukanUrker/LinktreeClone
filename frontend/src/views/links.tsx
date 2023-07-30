8651b5cfed15ccfe46aa2a8390220aefc3e645fa
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-30 22:25:44 +0300

    background gradient effects changed

--- frontend/src/views/links.tsx
+++ frontend/src/views/links.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { colorL, colorR } from "../utils/randomColor";
+import { gradient } from "../utils/randomGradient";
 export default function Links() {
   let { userName } = useParams();
   const [data, setData] = useState([]);
@@ -16,7 +16,7 @@ export default function Links() {
         <a
           href={link.link}
           target="_blank"
-          className="w-36 p-4 m-4 rounded-md transition duration-150 ease-in-ou block font-medium text-lg"
+          className="w-36 p-4 m-4 mx-auto rounded-md transition duration-150 ease-in-ou block font-medium text-lg hover:scale-125"
           style={{ backgroundColor: link.bgColor, color: link.textColor }}
         >
           {link.text}
@@ -28,15 +28,11 @@ export default function Links() {
   return (
     <>
       <div
-        className={`bg-gradient-to-br ${colorL} ${colorR} select-none text-center`}
+        className={`${gradient} select-none text-center grid place-items-center h-screen w-screen`}
       >
-        <div className="absolute left-[50%] mt-8">
-          <h1 className="font-medium text-4xl relative left-[-50%]">
-            {userName}
-          </h1>
-        </div>
-        <div className=" container  grid h-screen place-items-center w-fit mx-auto">
-          <div>{renderLinks()}</div>
+        <div className="w-fit">
+          <h1 className="font-semibold text-3xl mb-8 ">{userName}</h1>
+          {renderLinks()}
         </div>
       </div>
     </>
