e1e3398e3f69e22273818b687b3ae571bae2a0d1
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-30 22:23:51 +0300

    index page added

--- frontend/src/views/index.tsx
+++ frontend/src/views/index.tsx
@@ -1,8 +1,18 @@
+import image from "../assets/iphone14prolinktreeclone.png";
 export default function index() {
   return (
     <>
-      <div className="">
-        <h1>home page</h1>
+      <div className="w-fit mx-auto select-none">
+        <img src={image} alt="" className="w-72 mt-12 lg:w-96 h-fit mb-4" />
+      </div>
+      <div className="w-fit mx-auto">
+        {" "}
+        <a
+          href="/register"
+          className="text-center text-xl text-indigo-500 hover:text-indigo-600 rounded-lg  duration-150 p-2"
+        >
+          Click for create link pages
+        </a>
       </div>
     </>
   );
