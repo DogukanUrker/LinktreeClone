592cbed95df05f89d55458f36a17cf179f84f35c
Doğukan Ürker <dogukanurker@icloud.com>
2023-07-31 23:00:35 +0300

    auto navigating user to /profile when user already logged in

--- frontend/src/views/index.tsx
+++ frontend/src/views/index.tsx
@@ -1,25 +1,36 @@
 import image from "../assets/iphone14prolinktreeclone.png";
+import { fetchToken } from "../utils/auth";
+import { Navigate } from "react-router";
+
 export default function index() {
   return (
-    <>
-      <div className="w-fit mx-auto select-none">
-        <a href="/links/DogukanUrker" target="_blank">
-          <img
-            src={image}
-            alt="DogukanUrker's Links"
-            className="lg:mt-8 mt-4 lg:w-96 mb-4 w-[75%] h-[75%] mx-auto md:w-80"
-          />
-        </a>
+    <div>
+      <div className="grid h-screen place-items-center select-none">
+        {fetchToken() ? (
+          <Navigate to="/profile" />
+        ) : (
+          <div>
+            <div className="w-fit mx-auto select-none">
+              <a href="/links/DogukanUrker" target="_blank">
+                <img
+                  src={image}
+                  alt="DogukanUrker's Links"
+                  className="lg:mt-8 mt-4 lg:w-96 mb-4 w-[75%] h-[75%] mx-auto md:w-80"
+                />
+              </a>
+            </div>
+            <div className="w-fit mx-auto">
+              {" "}
+              <a
+                href="/register"
+                className="text-center text-xl text-indigo-500 hover:text-indigo-600 rounded-lg  duration-150 p-2"
+              >
+                Click for create custom link page
+              </a>
+            </div>
+          </div>
+        )}
       </div>
-      <div className="w-fit mx-auto">
-        {" "}
-        <a
-          href="/register"
-          className="text-center text-xl text-indigo-500 hover:text-indigo-600 rounded-lg  duration-150 p-2"
-        >
-          Click for create custom link page
-        </a>
-      </div>
-    </>
+    </div>
   );
 }
